0a7437c92d3efea9f0ccb20ef3b8204a41cf66a2
voxx <cd@goldencomm.com>
2017-04-09 23:53:29 -0700

    Let's try this again.

--- PokeAlarm/Manager.py
+++ PokeAlarm/Manager.py
@@ -517,7 +517,7 @@ class Manager(object):
 
         attempts = 0
         if cp == '?':
-            pid = ditto_id if not ditto_id else pkmn_id
+            pid = ditto_id if ditto_id is not False else pkmn_id
             while True:
                 attempts += 1
                 try:
