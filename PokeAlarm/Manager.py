686bd005d8fe83b7a144e00af840a14e114a59c8
voxx <cd@goldencomm.com>
2017-04-21 14:37:18 -0700

    Working on ensuring ditto data is accurate. (DO NOT PULL YET.)

--- PokeAlarm/Manager.py
+++ PokeAlarm/Manager.py
@@ -595,8 +595,8 @@ class Manager(object):
             'charge_move': self.__move_name.get(charge_id, 'unknown'),
             'cp': "{}".format(str(cp)) if cp != '?' else '?',
             'level': "{}".format(str(level)) if level != '?' else '?',
-            'height': "{}".format(str(height)) if height != 'unkn' else 'unkn',
-            'weight': "{}".format(str(weight)) if weight != 'unkn' else 'unkn',
+            'height': "{}".format(str(round(height, 2))) if height != 'unkn' else 'unkn',
+            'weight': "{}".format(str(round(weight, 2))) if weight != 'unkn' else 'unkn',
             'gender': "{}".format(str(gender)) if gender != 'unknown' else '?'
         })
         self.add_optional_travel_arguments(pkmn)
